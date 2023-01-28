cc22b314db8b176e0e0567630469a0af4c3884a3
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-28 16:34:12 +0530

    fixed the next button

--- src/components/News.js
+++ src/components/News.js
@@ -13,22 +13,28 @@ export class News extends Component {
         pageSize:PropTypes.number,
         category: PropTypes.string,
     }
-    constructor(props){
-        super(props);    
+    capitalizeFirstLetter=(string)=> {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+      constructor(props){
+          super(props);    
         this.state = {
             articles : [],
             loading: true,
             page:1 , 
             totalResults: 0
         }
-        document.title = this.props.category;
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} -NewsToday`;
     }
+    results = 0
     async componentDidMount(){ // It is a life cycle method|| jab render run ho jata hai phir componentDidMount run hota hai
         // console.log("componentDidMount");
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ca58a9658db6407786c7e5fe0bd37dab&page=1&pagesize=${this.props.pageSize}`;
         this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json();
+        console.log(parsedData.totalResults);
+        this.results = parsedData.totalResults
         // console.log(parsedData.totalResults);
         this.setState({loading:false})
         this.setState({totalResults:parsedData.totalResults})
@@ -57,7 +63,6 @@ export class News extends Component {
             let data = await fetch(url);
             this.setState({loading:false})
             let parsedData = await data.json();
-            // console.log(parsedData);
             this.setState({
                 page: this.state.page +1,
                 articles:parsedData.articles
@@ -80,7 +85,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
             <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousclick}>&larr;Previous</button>
-            <button disabled={this.state.page+1 > Math.ceil(38/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextclick}>Next&rarr;</button>
+            <button disabled={this.state.page+1 > Math.ceil(this.results/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextclick}>Next&rarr;</button>
         </div>
       </div>
     </div>
